c0b57a433282251c913114c2ec1eb5bf6a611b5b
Tan Zhi Shan <24035177@myrp.edu.sg>
2026-01-27 12:51:09 +0800

    admin login

--- server.js
+++ server.js
@@ -46,27 +46,53 @@ app.use(
     })
 );
 
-const DEMO_USER = { id: 1, username: "admin", password: "admin123" };
+// const DEMO_USER = { id: 1, username: "admin", password: "admin123" };
 
-const jwt = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET;
+// const jwt = require("jsonwebtoken");
+// const JWT_SECRET = process.env.JWT_SECRET;
+
+// app.post("/login", async (req, res) => {
+//     const { username, password } = req.body;
+
+//     if (username !== DEMO_USER.username || password !== DEMO_USER.password) {
+//         return res.status(401).json({error: "Invalid credentials"})
+//     }
+
+//     // create a token using the JWT secret
+//     const token = jwt.sign(
+//         { id: DEMO_USER.id, username: DEMO_USER.username },
+//         JWT_SECRET,
+//         { expiresIn: "1h"}
+//     );
+
+//     res.json({ token });
+// });
+const users = [
+  { id: 1, role: "admin", username: "admin1", password: "admin123" },
+  { id: 2, role: "user", username: "user1", password: "user123" },
+  { id: 3, role: "user", username: "user2", password: "user456" }
+];
 
 app.post("/login", async (req, res) => {
-    const { username, password } = req.body;
+  const { role, username, password } = req.body;
 
-    if (username !== DEMO_USER.username || password !== DEMO_USER.password) {
-        return res.status(401).json({error: "Invalid credentials"})
-    }
+  // find user from the array
+  const user = users.find(u => u.role === role && u.username === username && u.password === password);
 
-    // create a token using the JWT secret
-    const token = jwt.sign(
-        { id: DEMO_USER.id, username: DEMO_USER.username },
-        JWT_SECRET,
-        { expiresIn: "1h"}
-    );
+  if (!user) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
 
-    res.json({ token });
+  // create JWT token
+  const token = jwt.sign(
+    { role: user.role, id: user.id, username: user.username },
+    JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+
+  res.json({ token });
 });
+
 
 //Middleware to protect routes
 function requireAuth(req, res, next) {
